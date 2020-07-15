f06084c73c1e2376c64fa4eac45e20e45e890464
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-15 20:20:27 +0200

    styling delete button

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -8,7 +8,7 @@
         v-for = "task in tasks"
         :key="task.title"
         @click="task.done = !task.done"
-        :class=" {'done' :task.done} "
+        :class=" {'done bg-blue-1' :task.done} "
         clickable
         v-ripple>
         <q-item-section avatar>
@@ -19,6 +19,17 @@
         </q-item-section>
         <q-item-section>
           <q-item-label> {{task.title}} </q-item-label>
+        </q-item-section>
+        <q-item-section
+          v-if="task.done"
+          side
+          >
+          <q-btn 
+            flat
+            round
+            dense
+            color="primary" 
+            icon="delete" />
         </q-item-section>
       </q-item>
     </q-list>
