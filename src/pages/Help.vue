d7bca706b6febd22f1b28d29a8ea87cfa17662d7
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-15 19:49:20 +0200

    checkbox added

--- src/pages/Help.vue
+++ src/pages/Help.vue
@@ -1,6 +1,7 @@
 <template>
-  <q-page>
-    <h5>help</h5>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">help</h5>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius dolores soluta quo eos doloremque. Dolorum corrupti repellat reiciendis non culpa, facere eos, quo eum nostrum, voluptatum minima repellendus velit magnam!</p>
   </q-page>
 </template>
 
