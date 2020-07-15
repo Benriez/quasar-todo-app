68274feeb79585d7591ca87a5362f7464ac7fec9
Benriez <33022072+Benriez@users.noreply.github.com>
2020-07-15 20:32:13 +0200

    Dialog plugin installed

--- quasar.conf.js
+++ quasar.conf.js
@@ -88,7 +88,7 @@ module.exports = configure(function (/* ctx */) {
       importStrategy: 'auto',
 
       // Quasar plugins
-      plugins: []
+      plugins: ['Dialog']
     },
 
     // animations: 'all', // --- includes all animations
